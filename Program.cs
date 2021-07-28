186957a9d9fcafd4955fbf0ed86795b1b64918bc
Demetra Theodorou <demetra.theodoru@gmail.com>
2021-07-28 19:48:27 +0300

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coinCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
